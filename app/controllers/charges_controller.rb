67f694f9697ef4cb373cd8e644e6693174ce0c03
JaiswalAnkita <ankita.enbake@gmail.com>
2017-05-17 18:17:07 +0530

    showing all records

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,24 +1,31 @@
 class ChargesController < ApplicationController
   def new
-end
+  end
 
-def create
-  @amount = 500
+  def create
+  	@amount = 500
 
   customer = Stripe::Customer.create(
-    email: params[:stripeEmail],
-    source: params[:stripeToken]
+    :email => params[:stripeEmail],
+    :source  => params[:stripeToken]
   )
 
   charge = Stripe::Charge.create(
-    customer: customer.id,
-    amount: @amount,
-    description: 'Rails Stripe customer',
-    currency: 'usd'
+    :customer    => customer.id,
+    :amount      => @amount,
+    :description => 'Rails Stripe customer',
+    :currency    => 'usd'
   )
-
+  byebug
+  @charge = Charge.new
+   @charge.customer_id = customer.id
+   @charge.save
 rescue Stripe::CardError => e
   flash[:error] = e.message
   redirect_to new_charge_path
-end
+  end
+  def index
+  	@stripe_customres = Stripe::Customer.all
+  	
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :charges, only: [:new, :create]
+  resources :charges
   get 'thanks', to: 'charges#thanks', as: 'thanks'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'charges#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
